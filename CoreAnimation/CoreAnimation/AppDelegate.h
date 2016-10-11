d502c13930d9ae9515aa0ab1d5aa3b76d48c93d5
cuijinxin <cuijinxin@etouch.com>
2016-10-11 17:08:51 +0800

    init

---
+++ CoreAnimation/CoreAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreAnimation
+//
+//  Created by etouch on 16/10/11.
+//  Copyright © 2016年 EL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreAnimation/CoreAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreAnimation
+//
+//  Created by etouch on 16/10/11.
+//  Copyright © 2016年 EL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
